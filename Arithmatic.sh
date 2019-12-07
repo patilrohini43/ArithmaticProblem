4c01a92832c465a1eb1e8b7ae1652b0d69bbeae4
rohini <patilrohini43@gmail.com>
2019-12-07 17:10:09 +0530

    compute operation a * b + c

--- Arithmatic.sh
+++ Arithmatic.sh
@@ -2,7 +2,9 @@ echo "Welcome to Arithmatic Operation"
 read -p "Enter First Input" a
 read -p "Enter Second Input" b
 read -p "Enter Third Input" c
-result=$(( a + b * c ))
-echo $result
+cal1=$(( a + b * c ))
+cal2=$(( a * b + c ))
+echo $cal1
+echo $cal2
 
 
